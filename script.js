8a29cdbd3248e324422e2cd2c72353f7c6cbaf44
Tifannie Truman <nietru143@gmail.com>
2023-01-23 14:54:06 -0500

    added some js properties with values to the var password text option inside generate password function

--- script.js
+++ script.js
@@ -3,15 +3,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // This is inside the dashed bordered box on the webpage that displays Your Secure Password.
-function generatePassword() {
-  var uppercaseChoice = prompt('Please choose one Uppercase character, and click "ok".');
-  var lowercaseChoice = prompt('Please choose one lowercase character, and click "ok".');
-  var specialChoice = prompt('Please choose one special character, *&^%! etc..., and click "ok".');
-  var numberChoice = prompt('Please choose one numerical value character, and click "ok".');
 
-  var length = prompt("Please choose the length of your desired Passcode. (Must be 8 - 128 Characters)");
-  return "Password will be right here once I figure it out! haha"
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -22,5 +16,22 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ function generatePassword(length, options) {
+    var passwordText = {
+        numberChoice: "1234567890",
+        lowercaseChoice: "abcdefghijklmnopqrstuvwxyz",
+        uppercaseChoice: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        specialChoice: "!@#$%^&*()"
+    };
+    var uppercaseChoice = prompt('Please choose one Uppercase character, and click "ok".');
+    var lowercaseChoice = prompt('Please choose one lowercase character, and click "ok".');
+    var specialChoice = prompt('Please choose one special character, *&^%! etc..., and click "ok".');
+    var numberChoice = prompt('Please choose one numerical value character, and click "ok".');
+  
+    var length = prompt("Please choose the length of your desired Passcode. (Must be 8 - 128 Characters)");
+    return password;
+}
+
+var password = generatePassword(8, {digits: true, lowercase: true, uppercase: true, symbols: true});
+
+console.log(password);
