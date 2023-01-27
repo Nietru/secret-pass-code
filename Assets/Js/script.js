f8ef1633209a79980b00499fc1e42808f09a2891
Tifannie Truman <nietru143@gmail.com>
2023-01-26 23:13:22 -0500

    just fixed a bad href connecting the css and the js files to the html file

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -8,6 +8,7 @@ function generatePassword() {
       lowercaseChoice = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'].split(',');
       uppercaseChoice = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'].split(',');
       specialChoice = ['!,@,#,$,%,^,&,*,(,)'].split(',');
+      
       var charInputArray = []; // to store the user's selected characters.
 
       var pwLength = getPasswordLength(); //function to figure out users answer on length btwn 8 and 128 characters.
